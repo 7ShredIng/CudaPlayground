9f8873ed8e5464d72e79dc9c6e954c8a6e8186bd
7ShredIng <sickguttural@directbox.com>
2016-01-29 21:40:33 +0100

    vector sum in threads
    changed to multiple threads instead of blocks

--- VectorSum/VectorSum/kernel.cu
+++ VectorSum/VectorSum/kernel.cu
@@ -1,10 +1,24 @@
 #include "common.h"
+#include <Windows.h>    
+#include <conio.h>
 
-#define N 10
+#define N 5000
+
+typedef unsigned long long timestamp_t;
+double getRealTime()
+{
+	FILETIME tm;
+	ULONGLONG t;
+	GetSystemTimeAsFileTime(&tm);
+	t = ((ULONGLONG)tm.dwHighDateTime << 32) | (ULONGLONG)tm.dwLowDateTime;
+	return (double)t / 10000000.0;
+}
+
 
 __global__ void add(int *a, int *b, int *c)
 {
-	int tid = blockIdx.x;
+	//int tid = blockIdx.x; //in N blocks
+	int tid = threadIdx.x; //in N threads
 	if(tid<N)
 	{
 		c[tid] = a[tid] + b[tid];
@@ -15,6 +29,7 @@ int main(void)
 {
 	int a[N], b[N], c[N];
 	int *dev_a, *dev_b, *dev_c;
+	double startTime, endTime;
 
 	HANDLE_ERROR(cudaMalloc((void**)&dev_a, N*sizeof(int)));
 	HANDLE_ERROR(cudaMalloc((void**)&dev_b, N*sizeof(int)));
@@ -25,18 +40,23 @@ int main(void)
 		a[i] = -i;
 		b[i] = i * i;
 	}
-	
+	startTime = getRealTime();
+
 	HANDLE_ERROR(cudaMemcpy(dev_a, a, N*sizeof(int), cudaMemcpyHostToDevice));
 	HANDLE_ERROR(cudaMemcpy(dev_b, b, N*sizeof(int), cudaMemcpyHostToDevice));
 
-	add<<<N,1>>>(dev_a, dev_b, dev_c);
+	add<<<1,N>>>(dev_a, dev_b, dev_c); //in N threads
+	//add<<<N,1>>>(dev_a, dev_b, dev_c); //in N blocks
 
 	HANDLE_ERROR(cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost));
+
+	endTime = getRealTime();
 
 	for(int i=0; i<N; i++)
 	{
 		printf("%d + %d = %d\n", a[i], b[i], c[i]);
 	}
+	printf("Time used: %0.20lf\n", (endTime - startTime));
 
 	cudaFree(dev_a);
 	cudaFree(dev_b);
