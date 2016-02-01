7a3702e57d3aad6ce545f40aecdd981cb332892a
7ShredIng <sickguttural@directbox.com>
2016-02-01 22:35:40 +0100

    2D Texture
    using two dimensional texture memory

--- HeatingModel/HeatingModel/kernel.cu
+++ HeatingModel/HeatingModel/kernel.cu
@@ -1,12 +1,20 @@
-#include <cuda.h>
+#include "cuda.h"
 #include <common.h>
 #include <cpu_anim.h>
+#include <device_launch_parameters.h>
+#include "cuda_runtime.h"
+#include "cuda_texture_types.h"
+#include "texture_types.h"
 
 #define DIM 1024
 #define SPEED 0.25f
 #define PI 3.1415926535897932f
 #define MAX_TEMP 1.0f
 #define MIN_TEMP 0.0001f
+
+texture<float, cudaTextureType2D, cudaReadModeElementType> texConstSrc;
+texture<float, cudaTextureType2D, cudaReadModeElementType> texIn;
+texture<float, cudaTextureType2D, cudaReadModeElementType> texOut;
 
 struct DataBlock
 {
@@ -20,19 +28,20 @@ struct DataBlock
 	float frames;
 };
 
-__global__ void copy_const_kernel(float* iptr, const float* cptr)
+__global__ void copy_const_kernel(float* iptr)
 {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
 	int offset = x + y * blockDim.x *gridDim.x;
 
-	if(cptr[offset] != 0)
+	float c = tex2D(texConstSrc, x, y);
+	if(c != 0)
 	{
-		iptr[offset] = cptr[offset];
+		iptr[offset] = c;
 	}
 }
 
-__global__ void blend_kernel(float* outSrc, const float* inSrc)
+__global__ void blend_kernel(float* dst, bool dstOut)
 {
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
@@ -46,9 +55,7 @@ __global__ void blend_kernel(float* outSrc, const float* inSrc)
 	}
 	if(x == (DIM - 1))
 	{
-		right--;
-	}
-
+		 
 	int top = offset - DIM;
 	int bottom = offset + DIM;
 	if(y == 0)
@@ -60,7 +67,22 @@ __global__ void blend_kernel(float* outSrc, const float* inSrc)
 		bottom -= DIM;
 	}
 
-	outSrc[offset] = inSrc[offset] + SPEED * (inSrc[top] + inSrc[bottom] + inSrc[left] + inSrc[right] - inSrc[offset]*4);
+	float t, l, c, r, b;
+	if(dstOut)
+	{
+		t = tex2D(texIn, x, y-1);
+		l = tex2D(texIn, x-1, y);
+		c = tex2D(texIn, x, y);
+		r = tex2D(texIn, x+1, y);
+		b = tex2D(texIn, x, y+1);
+	}else{
+		t = tex2D(texOut, x, y-1);
+		l = tex2D(texOut, x-1, y);
+		c = tex2D(texOut, x, y);
+		r = tex2D(texOut, x+1, y);
+		b = tex2D(texOut, x, y+1);
+	}
+	dst[offset] = c + SPEED * (t + b + r + l - 4*c);
 }
 
 void anim_gpu(DataBlock* data, int ticks)
@@ -70,11 +92,21 @@ void anim_gpu(DataBlock* data, int ticks)
 	dim3 threads(16,16);
 	CPUAnimBitmap* bitmap = data->bitmap;
 
+	volatile bool dstOut = true;
 	for(int i = 0; i < 90; i++)
 	{
-		copy_const_kernel<<<blocks,threads>>>(data->dev_inSrc, data->dev_constSrc);
-		blend_kernel<<<blocks,threads>>>(data->dev_outSrc, data->dev_inSrc);
-		swap(data->dev_inSrc, data->dev_outSrc);
+		float *in, *out;
+		if(dstOut)
+		{
+			in = data->dev_inSrc;
+			out = data->dev_outSrc;
+		}else{
+			out = data->dev_inSrc;
+			in = data->dev_outSrc;
+		}
+		copy_const_kernel<<<blocks,threads>>>(in);
+		blend_kernel<<<blocks,threads>>>(out, dstOut);
+		dstOut = !dstOut;
 	}
 
 	float_to_color<<<blocks,threads>>>(data->output_bitmap, data->dev_inSrc);
@@ -91,6 +123,10 @@ void anim_gpu(DataBlock* data, int ticks)
 
 void anim_exit(DataBlock* data)
 {
+	HANDLE_ERROR(cudaUnbindTexture(texIn));
+	HANDLE_ERROR(cudaUnbindTexture(texOut));
+	HANDLE_ERROR(cudaUnbindTexture(texConstSrc));
+
 	cudaFree(data->dev_inSrc);
 	cudaFree(data->dev_outSrc);
 	cudaFree(data->dev_constSrc);
@@ -106,6 +142,7 @@ int main(void)
 	data.bitmap = &bitmap;
 	data.totalTime = 0;
 	data.frames = 0;
+
 	HANDLE_ERROR(cudaEventCreate(&data.start));
 	HANDLE_ERROR(cudaEventCreate(&data.stop));
 
@@ -113,6 +150,11 @@ int main(void)
 	HANDLE_ERROR(cudaMalloc((void**)&data.dev_inSrc, bitmap.image_size()));
 	HANDLE_ERROR(cudaMalloc((void**)&data.dev_outSrc, bitmap.image_size()));
 	HANDLE_ERROR(cudaMalloc((void**)&data.dev_constSrc, bitmap.image_size()));
+
+	cudaChannelFormatDesc desc = cudaCreateChannelDesc<float>(); 
+	HANDLE_ERROR(cudaBindTexture2D(NULL, texConstSrc, data.dev_constSrc, desc, DIM, DIM, sizeof(float)*DIM));
+	HANDLE_ERROR(cudaBindTexture2D(NULL, texIn, data.dev_inSrc, desc, DIM, DIM, sizeof(float)*DIM));
+	HANDLE_ERROR(cudaBindTexture2D(NULL, texOut, data.dev_outSrc, desc, DIM, DIM, sizeof(float)*DIM));
 
 	float* temp = (float*)malloc(bitmap.image_size());
 	for(int i = 0; i < DIM*DIM; i++)
